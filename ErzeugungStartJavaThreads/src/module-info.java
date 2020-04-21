6281836a8ece16af9554f1fb42d561b624edd889
Ghis <Gzeleu@protonmail.com>
2020-04-21 17:07:03 +0200

    Refactory Projects like in Online Seminar kurs

--- ErzeugungStartJavaThreads/src/module-info.java
+++
@@ -1,7 +0,0 @@
-module asd {
-	 // this module only needs types from the base module 'java.base';
-    // because every Java module needs 'java.base', it is not necessary
-    // to explicitly require it - I do it nonetheless for demo purposes
-    requires java.base;
-    exports fopt.buch;
-}
