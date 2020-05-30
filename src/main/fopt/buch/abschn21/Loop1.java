1dd6d0182027dc151991adb6e9b99f6ce117cf58
Ghislain <Gzeleu@gmx.de>
2020-05-31 00:09:25 +0200

    Private field with error jsu to test cricleci

--- src/main/fopt/buch/abschn21/Loop1.java
+++ src/main/fopt/buch/abschn21/Loop1.java
@@ -8,33 +8,33 @@ public class Loop1 extends Thread
 {
 
     // Name der jeweils Thread zu speichern.
-    private String myName;
+    private String myName1;
 
     public Loop1(String name)
     {
-        myName = name;
+	myName = name;
     }
 
     @Override
     public void run()
     {
-        for (int i = 1; i <= 100; i++)
-        {
-            System.out.println(myName + " (" + i + ")");
-        }
+	for (int i = 1; i <= 100; i++)
+	{
+	    System.out.println(myName + " (" + i + ")");
+	}
     }
 
     public static void main(String[] args)
     {
 
-        Loop1 t1 = new Loop1("Thread 1");
-        Loop1 t2 = new Loop1("Thread 2");
-        Loop1 t3 = new Loop1("Thread 3");
+	Loop1 t1 = new Loop1("Thread 1");
+	Loop1 t2 = new Loop1("Thread 2");
+	Loop1 t3 = new Loop1("Thread 3");
 
-        // die Start-Methode erweckt t1, t2, t3 zum Leben.
-        t1.start();
-        t2.start();
-        t3.start();
+	// die Start-Methode erweckt t1, t2, t3 zum Leben.
+	t1.start();
+	t2.start();
+	t3.start();
     }
 
 }
