e64bf3b1473d1696eaf91638c51c612742db1409
Ghislain <Gzeleu@gmx.de>
2020-05-30 15:20:02 +0200

    First Junit test using Jupiter module

---
+++ src/test/fopt/lehrmaterial/auf93/DisplayerTest.java
@@ -0,0 +1,40 @@
+package fopt.lehrmaterial.auf93;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class DisplayerTest
+{
+
+    @BeforeAll
+    static void setUpBeforeClass() throws Exception
+    {
+    }
+
+    @AfterAll
+    static void tearDownAfterClass() throws Exception
+    {
+    }
+
+    @BeforeEach
+    void setUp() throws Exception
+    {
+    }
+
+    @AfterEach
+    void tearDown() throws Exception
+    {
+    }
+
+    @Test
+    void test()
+    {
+	fail("Not yet implemented");
+    }
+
+}
