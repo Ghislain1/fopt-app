b9b8c4b3c34cb4d0ca8ca4d05cc77ed883d5be60
Ghislain <Gzeleu@gmx.de>
2020-06-01 16:59:15 +0200

    Fix typo errorin loop1 class

--- src/test/fopt/lehrmaterial/auf93/DisplayerTest.java
+++ src/test/fopt/lehrmaterial/auf93/DisplayerTest.java
@@ -1,12 +1,9 @@
 package fopt.lehrmaterial.auf93;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
 class DisplayerTest
 {
@@ -29,12 +26,6 @@ class DisplayerTest
     @AfterEach
     void tearDown() throws Exception
     {
-    }
-
-    @Test
-    void test()
-    {
-	fail("Not yet implemented");
     }
 
 }
