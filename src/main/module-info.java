a3124740a56bf1693116316a632bd1db3680636a
Ghislain <Gzeleu@gmx.de>
2020-06-01 15:05:20 +0200

    Separed test source file  and  source file, ide has been improved, build path

--- src/main/module-info.java
+++ src/main/module-info.java
@@ -1,5 +1,14 @@
 module fopt
 {
+    // Read-Abhaengigkeiten von fopt-app
     requires java.rmi;
-    requires org.junit.jupiter.api;
+
+    /*
+     * requires transitive org.apiguardian.api; requires transitive
+     * org.junit.platform.commons; requires transitive org.opentest4j;
+     */
+
+    // Exports von Packages von fopt-app
+    // exports fopt.buch.abschn2;
+    // exports fopt.buch.abschn621;
 }
