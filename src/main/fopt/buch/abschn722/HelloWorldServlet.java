a33dc21cc2338fc4116ee700590180bb99ec8491
Ghislain <Gzeleu@gmx.de>
2020-06-18 10:55:20 +0200

    Abschnitt 722 Not finish

---
+++ src/main/fopt/buch/abschn722/HelloWorldServlet.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package fopt.buch.abschn722;
+
+import HttpServlet;
+
+/**
+ * @author Zoe
+ * 
+ * @Findings HttpServlet not in JDK 13 why?
+ *
+ */
+public class HelloWorldServlet extends HttpServlet
+{
+
+}
