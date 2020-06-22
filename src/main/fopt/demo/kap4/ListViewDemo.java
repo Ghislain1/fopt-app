9fde7ee49a32e766953b7cc70379d9cd92e79883
Ghislain <Gzeleu@gmx.de>
2020-06-22 20:56:31 +0200

    Example of Listview in JavaX using  setCellFactory()-Method

--- src/main/fopt/demo/kap4/ListViewDemo.java
+++ src/main/fopt/demo/kap4/ListViewDemo.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.Scene;
+import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
@@ -23,13 +24,34 @@ public class ListViewDemo extends Application
         HBox hbox = new HBox();
 
         // Instanzoeren Listview Objekt
-        ListView<String> listview = new ListView<String>();
+        ListView<Person> listview = new ListView<Person>();
 
-        ObservableList<String> alphabet = FXCollections.observableArrayList("A", "B", "C");
-        listview.setItems(alphabet);
+        ObservableList<Person> people = FXCollections.observableArrayList(new Person("1", "Frank"), new Person("2", "Daniel"));
+
+        listview.setItems(people);
 
         // Listview anordnen
         hbox.getChildren().add(listview);
+
+        // Adapter
+        listview.setCellFactory(param -> new ListCell<Person>()
+        {
+            @Override
+            protected void updateItem(Person item, boolean empty)
+            {
+                super.updateItem(item, empty);
+
+                if (empty || item == null || item.getName() == null)
+                {
+                    setText(null);
+                }
+                else
+                {
+                    // Setup den Text, der anzuzeigen ist.
+                    setText(item.getName());
+                }
+            }
+        });
 
         Scene scene = new Scene(hbox, 300, 100);
         primStage.setTitle("Listview- Demo");
---
+++ src/main/fopt/demo/kap4/Person.java
@@ -0,0 +1,25 @@
+package fopt.demo.kap4;
+
+public class Person
+{
+    private String name;
+
+    private String id;
+
+    public Person(String id, String name)
+    {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public String getId()
+    {
+        return id;
+    }
+
+}
