0ee44aeeab9ebfc5236169588fac69a0a16a3bae
unknown <robbie.dailin@googlemail.com>
2020-08-17 00:03:18 +0200

    my solution for question 7a

--- src/main/fopt/klausur/lin/aufgabe7/Aufgabe7.java
+++ src/main/fopt/klausur/lin/aufgabe7/Aufgabe7.java
@@ -1,4 +1,31 @@
 package fopt.klausur.lin.aufgabe7;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Aufgabe7 {
+    public static void main(String[] args) {
+        System.out.println("----server start----");
+        boolean isRunning = true;
+        ServerSocket socket =null;
+        Socket client = null;
+        try {
+            socket = new ServerSocket(7777);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        while(isRunning) {
+            try {
+                client = socket.accept();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            System.out.println("------client build up a connection--------");
+
+            new Thread(new Channel(client)).start();
+
+        }
+    }
 }
