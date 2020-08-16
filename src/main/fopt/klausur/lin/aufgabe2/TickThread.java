a92a51bc526a249f50095cc83ed06ce78a1f5420
unknown <robbie.dailin@googlemail.com>
2020-08-16 20:00:28 +0200

    add sleep in run method to simulation delay

--- src/main/fopt/klausur/lin/aufgabe2/TickThread.java
+++ src/main/fopt/klausur/lin/aufgabe2/TickThread.java
@@ -10,6 +10,11 @@ public class TickThread implements Runnable {
     @Override
     public void run() {
         while (true ){
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             logicalTime.tick();
         }
     }
--- src/main/fopt/klausur/lin/aufgabe2/WaitTickThread.java
+++ src/main/fopt/klausur/lin/aufgabe2/WaitTickThread.java
@@ -13,6 +13,11 @@ public class WaitTickThread implements Runnable {
     @Override
     public void run() {
         while (true ){
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             logicalTime.waitTicks(waitTicks);
         }
     }
